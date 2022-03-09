29be58f792ca5f82722f05b6b7daa2bd32f78b39
Roman Fedorin <roman_fedorin@senla.eu>
2022-03-09 14:51:08 +0300

    9.4.2 Key/Value ternary relationship

--- src/main/java/rv/fedorin/auction/model/Category.java
+++ src/main/java/rv/fedorin/auction/model/Category.java
@@ -7,11 +7,18 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.MapKeyColumn;
 import javax.persistence.OneToMany;
 
 /**
@@ -36,6 +43,18 @@ public class Category {
     @Builder.Default
     @OneToMany(mappedBy = "category")
     private Set<CategorizedItem> categorizedItems = new HashSet<>();
+
+//    @EqualsAndHashCode.Exclude
+//    @ToString.Exclude
+//    @Builder.Default
+//    @ManyToMany(cascade = CascadeType.PERSIST)
+//    @MapKeyColumn(name = "item_id")
+//    @JoinTable(
+//            name = "category_item",
+//            joinColumns = @JoinColumn(name = "category_id", nullable = true),
+//            inverseJoinColumns = @JoinColumn(name = "user_id", nullable = true)
+//    )
+//    private Map<Item, User> itemAddedBy = new HashMap<>();
 
     public void addCategorizedItem(CategorizedItem categorizedItem) {
         categorizedItems.add(categorizedItem);
